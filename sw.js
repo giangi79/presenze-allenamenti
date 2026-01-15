010d7d5c0aa0c8560f0e2d8895938ae6eb493e23
giangi79 <giangi79@gmail.com>
2026-01-15 21:19:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installato');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Lasciamo che le richieste passino normalmente
+  e.respondWith(fetch(e.request));
+});
